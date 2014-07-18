f957f62f6b2e9fded9bb84e774a0f0efb379d632
jialiya <jialiya.huang0@gmail.com>
2014-07-18 12:11:15 -0700

    added timeouts

--- index.js
+++ index.js
@@ -18,9 +18,14 @@ function Wifi(){
     // { ssid: 
     //   , password: optional only if security == "unsecured"
     //   , security: defaults to wpa2 
+    //   , timeout: defaults to 60s
     // }
 
+    var connectionTimeout;
+
     options.security = (options.security && options.security.toLowerCase()) || "wpa2";
+
+    options.timeout = options.timeout || 60;
 
     if (!options || !options.ssid) {
       throw Error("No SSID given");
@@ -30,10 +35,23 @@ function Wifi(){
       throw Error("No password given for a network with security type", options.security);
     }
 
-    self._connectOpts = options;
+    // initiate connection
+    var ret = hw.wifi_connect(options.ssid, options.password, options.security);
+
+    if (ret != 0) {
+      process.removeListener('wifi_connect_complete', callback);
+
+      self._failProcedure("Previous wifi connect is in the middle of a call", callback);
+    } else {
+      connectionTimeout = setTimeout(function(){
+        self.emit('timeout', null);
+        callback && callback("Connection timed out");
+      }, options.timeout * 1000);
+    }
 
     if (callback) {
       process.once('wifi_connect_complete', function(err, data){
+        clearTimeout(connectionTimeout);
         if (!err) {
           callback(err, JSON.parse(data));
         } else {
@@ -43,36 +61,23 @@ function Wifi(){
     }
     
     process.on('wifi_connect_complete', function(err, data){
+      clearTimeout(connectionTimeout);
       if (!err) {
         self.emit('connect', err, JSON.parse(data))
       } else {
         self.emit('disconnect', err, data)
       }
     })
-    
-    // initiate connection
-    var ret = hw.wifi_connect(options.ssid, options.password, options.security);
-
-    if (ret < 0) {
-      process.removeListener('wifi_connect_complete', callback);
-
-      var errMsg = "Previous wifi connect is in the middle of a call";
-      if (callback) {
-        callback(new Error(errMsg));
-      } else {
-        new Error(errMsg);
-      }
-    }
 
     return self;
   }
 
   self.isConnected = function() {
-    return hw.wifi_is_connected();
+    return hw.wifi_is_connected() == 1 ? true : false;
   }
 
   self.isBusy = function(){
-    return hw.wifi_is_busy();
+    return hw.wifi_is_busy() == 1 ? true : false;
   }
 
   self.connection = function() {
@@ -102,24 +107,23 @@ function Wifi(){
       // disconnect
       var ret = hw.wifi_disconnect();
 
-      if (ret < 0) {
+      if (ret != 0) {
         process.removeListener('wifi_disconnect_complete', callback);
-
-        var errMsg = "Could not disconnect properly, wifi is currently busy.";
-        if (callback) {
-          callback(new Error(errMsg));
-        } else {
-          new Error(errMsg);
-        }
+        self._failProcedure("Could not disconnect properly, wifi is currently busy.", callback);
       }
 
     } else {
-      callback(new Error("Not connected"));
+      self._failProcedure("Cannot disconnect. Wifi is not currently connnected.", callback);
     }
   }
 
+  self._failProcedure = function (err, callback){
+    self.emit('error', err);
+    if (callback) callback(err);
+  } 
+
   self.isEnabled = function() {
-    return hw.wifi_is_enabled();
+    return hw.wifi_is_enabled() == 1 ? true : false;
   }
 
   self.disable = function() {
